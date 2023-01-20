3e756dd0cd36bda5fffd0047170a1ec614dbecc6
Serg <serg212007@gmail.com>
2023-01-20 14:47:40 +0200

    tsta

---
+++ src/components/App.jsx
@@ -0,0 +1,48 @@
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from '@mui/material';
+import { Suspense } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#f5f5f5',
+    },
+  },
+});
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+
+        <Box as="main" py={3}>
+          <Container maxWidth="lg">
+            <Suspense fallback={null}>
+              <Outlet />
+            </Suspense>
+          </Container>
+        </Box>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  );
+};
